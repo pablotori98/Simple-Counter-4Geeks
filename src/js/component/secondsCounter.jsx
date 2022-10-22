64ea2d84d5d61595891297be9eccae20c9dc3413
pablotori98 <pablotori98@gmail.com>
2022-10-22 12:03:33 +0000

    entrega

---
+++ src/js/component/secondsCounter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SecondsCounter = (props) =>{
+
+        return (<div className="contador"><h1>{props.contador}</h1></div>)
+
+    
+}
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,15 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+import SecondsCounter from "./component/secondsCounter.jsx"
+
+let contador = 0;
+let interval = setInterval(() =>{
+    contador ++;
+    console.log(contador)
+    ReactDOM.render(<SecondsCounter contador={contador}/>, document.querySelector("#app"));
+
+}, 1000)
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
